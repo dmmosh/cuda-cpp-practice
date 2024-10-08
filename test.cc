866530b9926a118b8f5937808ad892c9386b632a
dmmosh <evilsharkeater@gmail.com>
2024-10-07 21:10:59 -0400

    nothing of note

--- test.cc
+++ test.cc
@@ -5,6 +5,15 @@
 CUDA PRACTICE 
 
 https://youtu.be/86FAWCzIe_4
+
+typical CUDA flow:
+1 cpu allocates memory
+2 cpu copies data to gpu
+3 cpu launched kernel on gpu 
+4 cpu copies from gpu back to cpu to do something with it
+
+cpu: host, execuses FUNCTIONS
+gpu: device, execuses KERNELS (functions)
 */
 
 int main(){
